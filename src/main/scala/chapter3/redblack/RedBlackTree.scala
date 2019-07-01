43fbd534023ae74f6b799fcbc99ea2599f9dfb38
matsu-chara <matsuy00@gmail.com>
2019-07-02 00:48:21 +0900

    impl stream

--- src/main/scala/chapter3/redblack/RedBlackTree.scala
+++ src/main/scala/chapter3/redblack/RedBlackTree.scala
@@ -54,4 +54,5 @@ object RedBlackTree {
       else n
   }
 
+  Stream
 }
---
+++ src/main/scala/chapter4/Memoizer.scala
@@ -0,0 +1,8 @@
+package chapter4
+
+object Memoizer {
+  def $[X, Y](f: X => Y): X => Y = {
+    val cache = scala.collection.mutable.Map[X, Y]()
+    x => cache.getOrElseUpdate(x, f(x))
+  }
+}
