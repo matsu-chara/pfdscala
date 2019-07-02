93b93b194234648e824f777cc0ca3338a3b7258b
matsu-chara <matsuy00@gmail.com>
2019-07-03 00:21:12 +0900

    impl queue

---
+++ src/main/scala/chapter5/Queue.scala
@@ -0,0 +1,23 @@
+package chapter5
+
+case class Queue[A](out: List[A], in: List[A]) {
+  // 不変条件: outがempty ならば inはempty
+
+  def head: A = out.head // queue全体がemptyのときのみ例外を投げる。
+
+  def tail: Queue[A] = this match {
+    case Queue(Nil, _) => throw new NoSuchElementException
+    case Queue(_ :: front, rear) => copy(out = front, in = rear).checkf
+  }
+
+  def snoc(x: A): Queue[A] = copy(out = out, in = x :: in).checkf
+
+  private def checkf: Queue[A] = this match {
+    case Queue(Nil, rear) => Queue(rear.reverse, Nil)
+    case _ => this
+  }
+}
+
+object Queue {
+  def empty[A]: Queue[A] = Queue(Nil, Nil)
+}
